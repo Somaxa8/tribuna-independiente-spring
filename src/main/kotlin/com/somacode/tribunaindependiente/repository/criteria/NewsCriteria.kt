9bc718fd814f5f844d85a6b25811980b29e65601
Somacode <silviofrancoxa8@gmail.com>
2021-06-25 15:31:56 -0500

    added featured filter

--- src/main/kotlin/com/somacode/tribunaindependiente/repository/criteria/NewsCriteria.kt
+++ src/main/kotlin/com/somacode/tribunaindependiente/repository/criteria/NewsCriteria.kt
@@ -1,6 +1,7 @@
 package com.somacode.tribunaindependiente.repository.criteria
 
 import com.somacode.tribunaindependiente.entity.News
+import com.somacode.tribunaindependiente.entity.NewsLabel
 import com.somacode.tribunaindependiente.entity.NewsLabel_
 import com.somacode.tribunaindependiente.entity.News_
 import com.somacode.tribunaindependiente.service.tool.CriteriaTool
@@ -8,7 +9,9 @@ import org.springframework.data.domain.Page
 import org.springframework.stereotype.Repository
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
+import javax.persistence.criteria.Join
 import javax.persistence.criteria.Path
+import javax.persistence.criteria.Predicate
 
 @Repository
 class NewsCriteria {
@@ -17,20 +20,26 @@ class NewsCriteria {
     lateinit var entityManager: EntityManager
 
 
-    fun findFilterPageable(page: Int, size: Int, search: String?, labelId: Long?): Page<News> {
+    fun findFilterPageable(page: Int, size: Int, search: String?, labelId: Long?, featured: Boolean = false): Page<News> {
         val cb = entityManager.criteriaBuilder
         val q = cb.createQuery(News::class.java)
         val news = q.from(News::class.java)
-
-        val n: Path<Set<String>> = news.get(News_.LABEL)
-        val label: Path<Set<String>> = n.get(NewsLabel_.ID)
         val order: Path<Set<String>> = news.get(News_.ID)
 
+        val predicates: MutableList<Predicate> = mutableListOf()
+        val label: Join<News, NewsLabel> = news.join(News_.label)
+
         labelId?.let {
-            q.select(news).where(cb.equal(label, labelId)).orderBy(cb.desc(order))
-        } ?: run {
-            q.select(news).where().orderBy(cb.desc(order))
+            predicates.add(cb.equal(label.get(NewsLabel_.id), labelId))
         }
+
+        if (featured) {
+            predicates.add(cb.equal(news.get(News_.featured), true))
+        }
+
+        q.select(news).where(
+                *predicates.toTypedArray()
+        ).orderBy(cb.desc(order))
 
         return CriteriaTool.page(entityManager, q, page, size)
     }
